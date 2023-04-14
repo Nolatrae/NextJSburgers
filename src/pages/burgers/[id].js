d425e799593e6f77ba7758493904db21b80579a5
Nolatrae <105476991+Nolatrae@users.noreply.github.com>
2023-04-14 15:16:39 +0800

    dynamic routes

--- src/pages/burgers/[id].js
+++ src/pages/burgers/[id].js
@@ -1,10 +1,14 @@
+import styles from '../../styles/Burger.module.css'
+import Image from 'next/image';
+
+
 export const getStaticPaths = async () => {
   const res = await fetch('http://localhost:5000/items')
   const data = await res.json()
 
-  const paths = data.map(burger =>{
+  const paths = data.map(burger => {
     return {
-      params: { id: burger.id}
+      params: { id: burger.id }
     }
   })
 
@@ -14,11 +18,34 @@ export const getStaticPaths = async () => {
   }
 }
 
+export const getStaticProps = async (context) => {
+  const id = context.params.id
+  const res = await fetch(`http://localhost:5000/items/${id}`)
+  const data = await res.json()
 
-const Details = () => {
+  return {
+    props: { burger: data }
+  }
+}
+
+
+const Details = ({ burger }) => {
   return (
-    <div>
-      <h1>Details</h1>
+    <div className={styles.singleBurger}>
+      <h1>{burger.name}</h1>
+      <div className={styles.imageContainer}>
+        <Image
+          src={`${burger.image}`}
+          alt={`${burger.name}`}
+          width={100}
+          height={100}
+          layout="responsive"
+          objectFit="cover"
+        />
+      </div>
+      <div>
+        <p>{ burger.desc }</p>
+      </div>
     </div>
   )
 }
